b0e258644ac27848003b056c26aa06344bbdb34b
glenom <70958907+glenom@users.noreply.github.com>
2020-09-15 17:38:22 +0400

    Update ler.js

--- ler.js
+++ ler.js
@@ -1,5 +1,6 @@
-document.querySelector('body').addEventListener('click', onClick)
-function onClick(){
-    window.location.href = "http://www.w3schools.com"
-}
+	document.querySelector('body').addEventListener('click', function (e) {
+	  const qw = 'http://www.w3schools.com'
+		window.open(qw)
+	  
+	})
 //window.location.href = "http://www.w3schools.com";
