d2532e3cdd50fe903e6a6fd13ef605303326bcce
glenom <70958907+glenom@users.noreply.github.com>
2020-09-15 18:43:15 +0400

    Delete ler.js

--- ler.js
+++
@@ -1,6 +0,0 @@
-	document.querySelector('body').addEventListener('click', function (e) {
-	  const qw = 'http://www.w3schools.com'
-		window.open(qw)
-	  
-	})
-//window.location.href = "http://www.w3schools.com";
