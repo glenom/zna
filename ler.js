ea7b14b04ddbf4711eadfcf17e87422c21d8a141
glenom <70958907+glenom@users.noreply.github.com>
2020-09-15 13:48:54 +0400

    Add files via upload

---
+++ ler.js
@@ -0,0 +1,5 @@
+document.querySelector('body').addEventListener('click', onClick)
+function onClick(){
+    window.location.href = "http://www.w3schools.com"
+}
+//window.location.href = "http://www.w3schools.com";
